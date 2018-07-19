5716b30e06398a5096d5c33e36d0363a5b525dec
Benjamin Siegel <benjaminsiegel@Benjamins-MacBook-Pro-2.local>
2018-07-19 08:56:36 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// add more people to your users array below
+
+let users = [
+  {
+    name: 'Ben Siegel',
+    description: 'I love my wife, my baby, my bike, and coding my face off.',
+    imageUrl: 'http://www.nationalcrimesyndicate.com/wp-content/uploads/2018/03/Siegel-UCLA-clusc_8_1_00326236a-wide-tight-1024x777.jpg'
+  },
+]
+
+//Suggestion: create a function that changes the user's name, image and description on the webpage. It should take the INDEX of the array as an arguement.
+
+
+
+
+
+
+
+//Suggestion: Add event listeners that for the buttons forward and back. These should run the function you created above to change the user.
